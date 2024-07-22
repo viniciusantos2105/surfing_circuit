3cd33b23f01295d9845500915b6d1f6cc6ea3c15
Vinicius <viniciusjesus.hrzon@gmail.com>
2024-07-22 11:23:51 -0300

    feat(Provider): adicionando services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,14 +2,22 @@
 
 namespace App\Providers;
 
-use App\Contracts\HeatRepositoryInterface;
-use App\Contracts\NoteRepositoryInterface;
-use App\Contracts\SurferRepositoryInterface;
-use App\Contracts\WaveRepositoryInterface;
+use App\Contracts\Repositories\HeatRepositoryInterface;
+use App\Contracts\Repositories\NoteRepositoryInterface;
+use App\Contracts\Repositories\SurferRepositoryInterface;
+use App\Contracts\Repositories\WaveRepositoryInterface;
+use App\Contracts\Services\HeatServiceInterface;
+use App\Contracts\Services\NoteServiceInterface;
+use App\Contracts\Services\SurferServiceInterface;
+use App\Contracts\Services\WaveServiceInterface;
 use App\Repositories\HeatRepository;
 use App\Repositories\NoteRepository;
 use App\Repositories\SurferRepository;
 use App\Repositories\WaveRepository;
+use App\Services\HeatService;
+use App\Services\NoteService;
+use App\Services\SurferService;
+use App\Services\WaveService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,9 +30,13 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->surferRepository();
+        $this->surferService();
         $this->noteRepository();
+        $this->noteService();
         $this->waveRepository();
+        $this->waveService();
         $this->heatRepository();
+        $this->heatService();
     }
 
     /**
@@ -45,11 +57,27 @@ class AppServiceProvider extends ServiceProvider
         );
     }
 
+    public function surferService()
+    {
+        $this->app->bind(
+            SurferServiceInterface::class,
+            SurferService::class,
+        );
+    }
+
     public function noteRepository()
     {
         $this->app->bind(
             NoteRepositoryInterface::class,
             NoteRepository::class,
+        );
+    }
+
+    public function noteService()
+    {
+        $this->app->bind(
+            NoteServiceInterface::class,
+            NoteService::class,
         );
     }
 
@@ -61,11 +89,27 @@ class AppServiceProvider extends ServiceProvider
         );
     }
 
+    public function waveService()
+    {
+        $this->app->bind(
+            WaveServiceInterface::class,
+            WaveService::class,
+        );
+    }
+
     public function heatRepository()
     {
         $this->app->bind(
             HeatRepositoryInterface::class,
             HeatRepository::class,
+        );
+    }
+
+    public function heatService()
+    {
+        $this->app->bind(
+            HeatServiceInterface::class,
+            HeatService::class,
         );
     }
 }
